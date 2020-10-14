821dd3bfecb5cbde6fd715afe9fdeb33dbbf78f1
DimaKonoval <konoval6991@gmail.com>
2020-10-14 09:24:28 +0300

    Add button that load additional 20 cards, when clicked

--- src/App.js
+++ src/App.js
@@ -6,28 +6,49 @@ import './App.css';
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { isLoading: false, cards: [] };
+		this.state = { isLoading: false, cards: [], page: 1, totalPages: 0 };
 	}
-	componentDidMount() {
+
+	fetchData = () => {
 		this.setState({ isLoading: true });
 		fetch(
-			`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=1`
+			`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&sort_by=popularity.desc&page=${this.state.page}`
 		)
 			.then((res) => {
 				return res.json();
 			})
 			.then((data) => {
-				return this.setState({ cards: data.results, isLoading: false });
+				return this.setState((state) => ({
+					cards: [...state.cards, ...data.results],
+					isLoading: false,
+					totalPages: data.total_pages,
+					page: data.page,
+				}));
 			});
+	};
+	componentDidMount() {
+		this.fetchData();
 	}
+
+	loadMore = () => {
+		// e.preventDefault();
+		console.log('click');
+		const currPage = this.state.page;
+		this.setState({ page: currPage + 1 }, () => {
+			this.fetchData();
+		});
+	};
 
 	render() {
 		if (this.state.isLoading) {
 			return <h3>Loading...</h3>;
 		}
 		return (
-			<div>
+			<div className='App'>
 				<MovieListComponent cards={this.state.cards} />
+				<button onClick={this.loadMore} className='loadMore'>
+					Load More
+				</button>
 			</div>
 		);
 	}
