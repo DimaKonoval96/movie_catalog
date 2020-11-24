86636577e98809683dbd9cc6141e1047ead4f4bd
DimaKonoval <konoval6991@gmail.com>
2020-11-24 19:22:49 +0200

    Added runtime, overview and tagline on screen

--- src/pages/DetailsPage/DetailsPage.jsx
+++ src/pages/DetailsPage/DetailsPage.jsx
@@ -14,13 +14,18 @@ function DetailsPage(props) {
 		backdrop_path: backdropPath,
 		release_date: releaseDate,
 		release_dates: releaseDates,
+		genres,
+		runtime,
+		tagline,
+		overview,
+		credits,
 	} = cardDetails;
 
 	useEffect(() => {
 		let unmounted = false;
 		setLoading(true);
 		fetch(
-			`https://api.themoviedb.org/3/movie/${cardId}?api_key=${API_KEY}&language=en-US&append_to_response=release_dates`
+			`https://api.themoviedb.org/3/movie/${cardId}?api_key=${API_KEY}&language=en-US&append_to_response=release_dates,credits`
 		)
 			.then((res) => {
 				return res.json();
@@ -36,11 +41,15 @@ function DetailsPage(props) {
 		};
 	}, []);
 
+	const getCrewMembers = () => {
+		console.log(credits.crew);
+	};
+
 	const getReleaseYear = () => {
 		return releaseDate.split('-')[0];
 	};
 
-	// Search for release details based on release date
+	// Search for and return certification based on release date
 	const getCertification = () => {
 		const res = releaseDates.results.find(
 			(releaseObj) => releaseObj['iso_3166_1'] === 'US'
@@ -50,10 +59,28 @@ function DetailsPage(props) {
 	};
 
 	//Change date's format from yyyy-mm-dd to mm/dd/yyyy
-	const formatDate = () => {
+	const getReleaseDate = () => {
 		const [year, month, day] = releaseDate.split('-');
 		return [month, day, year].join('/');
 	};
+
+	// Retrieve from api response and return list of genres
+	const getGenresList = () => {
+		return genres.reduce((acc, obj) => {
+			acc.push(obj.name);
+			return acc;
+		}, []);
+	};
+
+	// Take runtime from state and return in '1h 1m' format
+	const getRuntime = () => {
+		let hours = Math.floor(runtime / 60);
+		let minutes = runtime % 60;
+		hours = hours > 0 ? `${hours}h` : '';
+		minutes = minutes > 0 ? `${minutes}m` : '';
+		return `${hours} ${minutes}`;
+	};
+
 	// Conditional rendering
 	if (isLoading) {
 		return <div>Loading...</div>;
@@ -78,15 +105,27 @@ function DetailsPage(props) {
 								title={`${title}`}
 							/>
 						)}
-						<div className='mainInfo'>
+						<section className='mainInfo'>
 							<h1 className='title'>
 								{title} <span>({releaseDate && getReleaseYear()})</span>
 							</h1>
 							<div>
-								<span>{getCertification()} </span>
-								<span>{formatDate()}</span>
+								<span className='certification'>
+									{getCertification() || '16+'}{' '}
+								</span>
+								<span>{getReleaseDate()}</span>
+								<div className='dot'></div>
+								<span>{getGenresList().join(', ')}</span>
+								<div className='dot'></div>
+								<span>{getRuntime()}</span>
 							</div>
-						</div>
+							<p className='tagline'>{tagline}</p>
+							<div className='overview'>
+								<h4>Overview</h4>
+								<p>{overview}</p>
+							</div>
+							<div className='crew'>{getCrewMembers()}</div>
+						</section>
 					</div>
 					<div className='overlay'></div>
 				</div>
